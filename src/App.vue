5bb7e313edffe68a7eeb84ae7b4b1dab1b764d5c
zelenka21 <gytis.zelenka@gmail.com>
2020-01-07 21:18:27 +0200

    logic for image pull added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,64 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div class="container">
+      <div class="button-wrapper">
+        <button class="btn" @click="searchUnsplash('car')">Cars</button>
+      </div>
+      <stack
+              :column-min-width="300"
+              :gutter-width="15"
+              :gutter-height="15"
+              monitor-images-loaded
+      >
+        <stack-item
+                v-for="(image, i) in images"
+                :key="i"
+                style="transition: transform 300ms"
+        >
+          <img :src="image.urls.small" :alt="image.alt_description" />
+        </stack-item>
+      </stack>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import { Stack, StackItem } from "vue-stack-grid";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Stack,
+    StackItem
+  },
+  data: () => ({
+    images: []
+  }),
+  methods: {
+    searchUnsplash(topic) {
+      this.images = [];
+      axios
+        .get(
+          `https://api.unsplash.com/search/photos?query=${topic}&per_page=20`,
+          {
+            headers: {
+              Authorization:
+                "Client-ID a6230bba66b123cf708eea39b974041b2be29fef713b30538a84d84de8df89a6",
+              "Accept-Version": "v1"
+            }
+          }
+        )
+        .then(response => {
+          this.images = response.data.results;
+        })
+        .catch(() => {
+          this.images = [];
+        });
+    }
   }
-}
+};
+
 </script>
 
 <style>
@@ -24,5 +69,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  width: 80vw;
+  margin: 0 auto;
+}
+.button-wrapper {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 25px;
+}
+.btn {
+  font-size: 18px;
+  background-color: #42b983;
+  color: white;
+  padding: 10px 20px;
 }
 </style>
